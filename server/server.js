a6c2cbb6297bbd4547d4a48d61913f67282577d6
Caroline Fenlon <carfenlon@gmail.com>
2018-06-14 16:24:58 +0100

    Added GET /todo/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -25,6 +26,25 @@ app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos}); // send object instead of just array, then additional variables can be added in future
     }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => { // :id creates id variable on request object
+    // res.send(req.params); // to check params sent in GET 
+    var id = req.params.id;
+
+    // validate ID
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            res.send({todo});
+        } else {
+            res.status(404).send();
+        }
+    }).catch((e) => {
         res.status(400).send(e);
     });
 });
